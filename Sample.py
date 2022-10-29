7b7d84df11273fd5331e792cfabf0a0b5295077c
Ángel Barruz <a.barruz@gmail.com>
2022-10-29 09:07:20 +0200

    Final_Project

---
+++ Sample.py
@@ -0,0 +1,13 @@
+# Importamos Libreria streamlit
+
+import streamlit as st
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+
+with header:
+    st.title('Bsucador Gasolineras')
+
+
+
---
+++ sample2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+
+with header:
+    st.title('Bsucador Gasolineras')
